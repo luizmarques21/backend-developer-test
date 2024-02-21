952ec396f48e1f2eab4f92449507115ae89f55f1
Luiz Marques <mariel.marques21@gmail.com>
2024-02-21 17:31:41 -0300

    feat: adiciona novo metodo para buscar job pelo id

--- config/db.js
+++ config/db.js
@@ -17,20 +17,23 @@ async function connect() {
 async function listCompanies() {
     const client = await connect();
     const res = await client.query('SELECT * FROM companies');
+    client.release();
     return res.rows;
 }
 
 async function getCompany(companyId) {
     const client = await connect();
     const res = await client.query('SELECT * FROM companies WHERE id = $1', [companyId]);
+    client.release();
     return res.rows[0];
 }
 
 async function saveJobDraft(newJob) {
     const client = await connect();
-    const sql = 'INSERT INTO jobs(company_id, title, description, location, notes) VALUES ($1, $2, $3, $4, $5)';
-    const values = [newJob.company_id, newJob.title, newJob.description, newJob.location, newJob.notes];
+    const sql = 'INSERT INTO jobs(company_id, title, description, location) VALUES ($1, $2, $3, $4)';
+    const values = [newJob.company_id, newJob.title, newJob.description, newJob.location];
     await client.query(sql, values);
+    client.release();
 }
 
 async function updateJob(newJob) {
@@ -38,6 +41,7 @@ async function updateJob(newJob) {
     const sql = 'UPDATE jobs SET title = $1, location = $2, description = $3 WHERE id = $4';
     const values = [newJob.title, newJob.location, newJob.description, newJob.id];
     await client.query(sql, values);
+    client.release();
 }
 
 async function publishJob(job_id) {
@@ -45,6 +49,7 @@ async function publishJob(job_id) {
     const sql = 'UPDATE jobs SET status = $1 WHERE id = $2';
     const values = ['published', job_id];
     await client.query(sql, values);
+    client.release();
 }
 
 async function archiveJob(job_id) {
@@ -52,11 +57,20 @@ async function archiveJob(job_id) {
     const sql = 'UPDATE jobs SET status = $1 WHERE id = $2';
     const values = ['archived', job_id];
     await client.query(sql, values);
+    client.release();
 }
 
 async function deleteJob(job_id) {
     const client = await connect();
     await client.query('DELETE FROM jobs WHERE id = $1', [job_id]);
+    client.release();
+}
+
+async function getJob(jobId) {
+    const client = await connect();
+    const res = await client.query('SELECT * FROM jobs WHERE id = $1', [jobId]);
+    client.release();
+    return res.rows[0];
 }
 
 module.exports = { 
@@ -66,5 +80,6 @@ module.exports = {
     updateJob, 
     publishJob,
     archiveJob,
-    deleteJob
+    deleteJob,
+    getJob
 }
